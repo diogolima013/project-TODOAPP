80eec0ccabad752f9117ee4c2d9e0e062009d7f2
Diogo <diogoslva0133@gmail.com>
2022-11-18 09:01:59 -0300

    configuring the reducers

---
+++ src/main/reducers.js
@@ -0,0 +1,21 @@
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers ({
+    todo: () => ({
+        description: 'Ler Livro',
+        list: [{
+            _id: 1,
+            description: 'Pagar fatura do cartão',
+            done: true
+        }, {
+            _id: 2,
+            description: 'Reunião com a equipe ad 10:00',
+            done: false
+        }, {
+            _id: 3,
+            description: 'Consulta médicas na terça depois do almoço',
+            done: false
+        }]
+    })
+})
+ export default rootReducer
